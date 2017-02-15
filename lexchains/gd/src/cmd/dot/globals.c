902b86f655763a595ab310b05639448ad8b8c1b3
Matthew D <matthew.dangerfield@yandex.com>
2017-02-15 14:00:52 -0600

    deleted unecesary folder

--- lexchains/gd/src/cmd/dot/globals.c
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) AT&T Corp. 1994, 1995.
- * This code is licensed by AT&T Corp.  For the
- * terms and conditions of the license, see
- * http://www.research.att.com/orgs/ssr/book/reuse
- */
-
-#define EXTERN
-#include "dot.h"
--- lexchains/sgmlsent/catch_trash.c
+++
@@ -1,8 +0,0 @@
-catch_trash(l,w)
-char *l, *w;
-{
-
-  if(strcmp(l,"Ph.")==0 && strcmp(w,"D.")==0)return(1);
-  else if(strcmp(l,"Minneapolis-St.")==0 && strcmp(w,"Paul")==0)return(1);
-  else return(0);
-}
